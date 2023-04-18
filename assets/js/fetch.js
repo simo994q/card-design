835a9d3cfcf631c14f6b84f38edc3df4f201aca8
simo994q <simonkn1@hotmail.com>
2023-04-18 12:41:51 +0200

    wdqsdqwd

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -18,7 +18,8 @@ function addProductToDom(product) {
     <p class="productDesc">${product.description}</p>
     <h2 class="productPrice">$${product.price}</h2>
     <p class="productRating">${product.rating} / 5</p>
-    <button class="productButton" onclick="addToCart(${product.id}, '${product.title}')">Add to cart</button>
+    <button class="productButton" onclick="shoppingcart.increaseCartQuantity(${product.id}, ${product.price}, '${product.title}')">Add to cart</button>
+    <button class="productButton" onclick="shoppingcart.setDeleteItem(${product.id})">Remove</button>
     `
     containerElement.appendChild(productElement)
 };
@@ -31,22 +32,29 @@ getProducts().then((data) => {
 })
 
 
-function addToCart(id, title) {
-    let cart = { id: id, title: title }
-    localStorage.setItem('cart', JSON.stringify(cart))
-}
+// function addToCart(id, title) {
+//     let cart = { id: id, title: title }
+//     localStorage.setItem('cart', JSON.stringify(cart))
+// }
 
 
 function createCart() {
-    const shoppingCart = JSON.parse(localStorage.getItem('myCart')) || { cartItems: [] }
+    const cart = JSON.parse(localStorage.getItem('myCart')) || { cartItems: [] }
 
     function saveCart() {
-        localStorage.setItem('myCart', JSON.stringify(shoppingCart))
+        localStorage.setItem('myCart', JSON.stringify(cart))
     };
 
     return {
         getCartItems: function () {
             return cart.cartItems
+        },
+
+        setDeleteItem: function (id) {
+            cart.cartItems = cart.cartItems.filter(function (item) {
+                return item.id !== id
+            })
+            saveCart()
         },
 
         increaseCartQuantity: function (id, price, title) {
@@ -58,7 +66,10 @@ function createCart() {
              } else {
                 cart.cartItems.push({id: id, price: price, amount: 1, title: title})
              }
+             saveCart()
         },
     }
 
 };
+
+const shoppingcart = createCart()
